8e4b5f612ae72aa97abb7c17e782a64ab71db386
Fernando Fritd <ferfritd@gmail.com>
2020-10-26 20:03:23 -0300

    Return to game button set

--- src/shared/UI/Modal.js
+++ src/shared/UI/Modal.js
@@ -1,17 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { createPortal } from 'react-dom'
 
 import './Modal.css'
 
 export default function Modal({active, restartGameHandler}) {
+
+    const [returnToGame, setReturnToGame] = useState(false)
     
+    const returnToGameHandler = () => {
+        setReturnToGame(true)
+    }
+
+    useEffect(() => {
+        if(returnToGame && !active){
+            setReturnToGame(false)
+        }
+    }, [returnToGame, active])
+
     const content = 
-    <div className={`modal ${!active ? 'modal-closed': 'modal-active'}`}>
+    <div className={`modal ${!active || returnToGame ? 'modal-closed': 'modal-active'}`}>
         <div>
         <h2>Congrats, you win! You're awesome!</h2>
         </div>
         <div className="buttons">
             <button onClick={restartGameHandler}>Play Again</button>
+            <button onClick={returnToGameHandler}>Return to Game</button>
         </div>
     </div>
 
